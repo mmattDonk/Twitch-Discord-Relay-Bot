4fc06c4877b51aa51aee902dd77b04cfe37c0965
mmatt <matt@mmatt.net>
2021-06-28 16:50:45 -0500

    ✨

---
+++ forwarderreal.py
@@ -0,0 +1,32 @@
+import asyncio
+import discord
+
+from discord.ext import commands
+from discord.ext.commands import bot
+
+from twitchbot import Bot
+
+loop = asyncio.get_event_loop()
+
+import dotenv
+import os
+dotenv.load_dotenv()
+
+bot = commands.Bot(command_prefix="xd")
+Bot = Bot()
+
+TOKEN = os.environ.get("TOKEN")
+
+
+@bot.event
+async def on_message(message):
+    print(message.content)
+
+    if message.channel.id == 853517503913132033:
+        if not message.author.bot:
+            await Bot.discord_relay_thing_command(msg=message.content, username=message.author.name)
+
+
+loop.create_task(bot.start(TOKEN))
+loop.create_task(Bot.run())
+loop.run_forever()
---
+++ twitchbot.py
@@ -0,0 +1,57 @@
+import datetime
+from aiohttp import request
+import twitchio
+from twitchio.ext import commands
+
+import requests
+from discord import Webhook, RequestsWebhookAdapter
+
+import os
+
+import dotenv
+
+import json
+
+with open("config.json") as config_file:
+    config = json.load(config_file)
+
+dotenv.load_dotenv()
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            irc_token=os.environ.get("TwitchTOKEN"),
+            client_id=os.environ.get("client_id"),
+            nick=config["nickname"],
+            prefix=config["prefix"],
+            initial_channels=config["channels"],
+        )
+
+    async def event_ready(self):
+        print(f"Ready | {self.nick}")
+
+    async def event_message(self, message):
+        print(
+            f"[MESSAGE LOGS] ({message.channel.name}) "
+            + message.author.name
+            + " - "
+            + message.content
+        )
+
+        if message.author.name != "doobme":
+            webhook = Webhook.from_url(os.environ.get("webhook_url"), adapter=RequestsWebhookAdapter())
+
+            webhook.send(message.author.name + " - " + message.content)
+
+        await self.handle_commands(message)
+
+    @commands.command(name="ping", aliases=["ding"])    
+    async def test_command(self, ctx):
+        await ctx.send(f"FeelsDankMan 🔔 ding @{ctx.author.name}")
+
+    async def discord_relay_thing_command(self, msg, username):
+        for channel in self.initial_channels:
+            xd = self.get_channel(channel)
+            await xd.send(f"{username} - {msg}")
+
